0d9541fccbfea8a16611fb4f6f71619365feaf8d
Thomas Hull <thull@scottlogic.co.uk>
2016-03-08 16:28:27 +0000

    Add module loading to lib

--- lib.js
+++ lib.js
@@ -1,3 +1,5 @@
+var Promise = require("promise");
+
 var promiseMaker = function(taskOwner, task, params, dataTransform, log) {
     return new Promise(function(resolve, reject) {
         task.call(taskOwner, params, function(err,data) {

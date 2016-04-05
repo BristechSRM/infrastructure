9a69b8e5c2dddffb12d8119303f788751bcd2a51
Jason Ebbin <jebbin@scottlogic.co.uk>
2016-04-05 15:42:26 +0100

    Fetch actions and group with cards and members

--- TrelloSync/TrelloSync/Board.fs
+++ TrelloSync/TrelloSync/Board.fs
@@ -1,15 +1,23 @@
 ﻿module Board
     open Members
     open Cards
+    open Actions
+
     type TrelloBoard =
         { Members : TrelloMember []
-          Cards : BasicCard [] }
+          Cards : BasicCard [] 
+          Actions : (string * BasicAction []) [] }
 
     let fetchBoardAsync trelloCred = 
         async {
             let! cards = getAllRawTalkCards trelloCred
             let! membersMeta = getAllMembersAsync trelloCred
-
-            return {Members = membersMeta.Members; Cards = cards |> Array.map(fun x -> x.Card)}
+            let! cardsAndCommentActions = getActionsPerCardAsync trelloCred cards
+            return 
+                {
+                    Members = membersMeta.Members
+                    Cards = cards |> Array.map(fun x -> x.Card)
+                    Actions = cardsAndCommentActions |> Array.map(fun x -> (x.Card.Card.Id, x.Actions))
+                }
 
         }

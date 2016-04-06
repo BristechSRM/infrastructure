734e619dd1b765cb43f2bfb84eae1924bd4fbdf1
Jason Ebbin <jebbin@scottlogic.co.uk>
2016-04-06 12:37:01 +0100

    Change categorization of other cases to talk card detection only

--- TrelloSync/TrelloSync/Cards.fs
+++ TrelloSync/TrelloSync/Cards.fs
@@ -13,7 +13,7 @@
 
     type RawTrelloCard = 
         { Card : BasicCard
-          Groups : GroupCollection }
+          RegexGroups : GroupCollection }
 
     type TrelloCard = 
         { SpeakerName : string
@@ -25,25 +25,8 @@
           AdminId : string
           AdminEmail : string }
 
-    type CardType = 
-        | Unmatched of BasicCard
-        | EventDate of BasicCard
-        | Template of BasicCard
-        | UnparsedTalkCard of RawTrelloCard
-        | TalkCard of TrelloCard
-
     let expectedNumberOfGroupsInCardParse = 5
     let speakerNameGroup = 1
-
-    let nameContainsDate (cardName: string) = 
-        let monthNames = 
-            DateTimeFormatInfo.CurrentInfo.MonthNames 
-            |> Array.map (fun x -> x.ToUpperInvariant())
-            |> Array.filter (String.IsNullOrWhiteSpace >> not)
-        let cardNameUpped = cardName.ToUpperInvariant()
-
-        monthNames 
-        |> Array.exists (fun month -> cardNameUpped.Contains(month))
 
     let allGroupsMatched (groups : GroupCollection) = 
         groups.Count = expectedNumberOfGroupsInCardParse && not <| String.IsNullOrWhiteSpace (groups.[speakerNameGroup].Value)
@@ -52,28 +35,16 @@
         let m = Regex.Match(input, pattern)
         if (m.Success) then Some m.Groups else None
 
-    let categorizeCard (card : BasicCard)= 
+    let cardsWithTalkData (card : BasicCard)= 
         match card.Name with
-        | _ when card.Name.ToUpperInvariant().Contains("TEMPLATE") -> Template card
-        | _ when nameContainsDate card.Name -> EventDate card
-        | AllRegexGroups "(.*)\[(.*)\]\((.*)\)(.*)$" groups -> 
-            if allGroupsMatched groups then 
-                UnparsedTalkCard {Card = card; Groups= groups}
-            else 
-                Unmatched card
-        | _ -> Unmatched card
+        | AllRegexGroups "(.*)\[(.*)\]\((.*)\)(.*)$" groups when allGroupsMatched groups-> Some {Card = card; RegexGroups= groups}
+        | _ -> None
 
-    let getAllCards trelloCred = 
+    let getAllRawTalkCards trelloCred = 
         async {
             let uri =  Uri(sprintf "https://api.trello.com/1/boards/524ec750ed130abd230011ab/cards/open?fields=id,name,idMembers&key=%s&token=%s" trelloCred.Key trelloCred.Token)
             use webClient = new WebClient()
             let! rawCards = webClient.AsyncDownloadString(uri)
             let basicCards = JsonConvert.DeserializeObject<BasicCard []>(rawCards)
-            return basicCards |> Array.map(categorizeCard)        
-        }
-
-    let getAllRawTalkCards trelloCred = 
-        async {
-            let! allCards = getAllCards trelloCred
-            return allCards |> Array.choose(function | UnparsedTalkCard rawTalkCard -> Some rawTalkCard | _ -> None)
+            return basicCards |> Array.choose(cardsWithTalkData)        
         }

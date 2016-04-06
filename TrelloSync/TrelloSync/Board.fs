4f3c18d52a5cd8ade1a19b7cc296239a037dd754
Jason Ebbin <jebbin@scottlogic.co.uk>
2016-04-06 12:28:27 +0100

    Remove Async.StartChild

--- TrelloSync/TrelloSync/Board.fs
+++ TrelloSync/TrelloSync/Board.fs
@@ -7,13 +7,8 @@
 
     let fetchBoardAsync trelloCred = 
         async {
-            //StartChild lets you start multiple asyncs without blocking for the result
-            let! cardsAsync = Async.StartChild <| getAllRawTalkCards trelloCred
-            let! membersMetaAsync = Async.StartChild <| getAllMembersAsync trelloCred
-
-            //Now we block for the result
-            let! cards = cardsAsync
-            let! membersMeta = membersMetaAsync
+            let! cards = getAllRawTalkCards trelloCred
+            let! membersMeta = getAllMembersAsync trelloCred
 
             return {Members = membersMeta.Members; Cards = cards |> Array.map(fun x -> x.Card)}
 

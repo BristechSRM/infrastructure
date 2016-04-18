e84d97effd108c96dcd42fed7f565e9b89e80fd8
Jason Ebbin <jebbin@scottlogic.co.uk>
2016-04-18 15:57:29 +0100

    Create models for import and basic SrmApiClient

---
+++ TrelloSync/TrelloSync/SrmApiClient.fs
@@ -0,0 +1,56 @@
+﻿module SrmApiClient
+    open System
+    open ImportModels
+    open System.Text
+    open System.Net.Http
+    open Newtonsoft.Json
+    open Helpers
+    
+    let sessionsServiceUri = Uri("http://127.0.0.1:9000")
+    let commsServiceUri = Uri("http://127.0.0.1:9001")
+    let profilesEndpoint = Uri(sessionsServiceUri, "Profiles")
+    let sessionsEndpoint = Uri(sessionsServiceUri, "Sessions")
+    let threadsEndpoint = Uri(commsServiceUri, "Threads")
+
+    let httpClient = new HttpClient()
+    //TODO Error handling
+
+    let postStringAsync (uri : Uri) data = 
+        let content = new StringContent(data,Encoding.UTF8,"application/json")
+        httpClient.PostAsync(uri,content) |> Async.AwaitTask   
+
+    let postAsync' (uri : Uri) data = 
+        async {
+            let data = JsonConvert.SerializeObject(data)
+            let! response = postStringAsync uri data
+            return! response.Content.ReadAsStringAsync() |> Async.AwaitTask
+        }
+    
+    module Profiles = 
+        let postAsync (profile : Profile) = 
+            async {
+                let! result = postAsync' profilesEndpoint profile 
+                return parseQuotedGuid result
+            }
+
+        let postAndGetIdAsync (profile: Profile) = 
+            async {
+                let! newId = postAsync profile
+                return {profile with Id = newId}    
+            }
+    
+    module Sessions = 
+        let postAsync (data : SessionData) = 
+            async { 
+                let! result = match data.AdminId with
+                              | Some aId -> postAsync' sessionsEndpoint <| cardToSession data.TrelloCard data.SpeakerId aId data.ThreadId
+                              | None -> postAsync' sessionsEndpoint <| cardToNoAdminSession data.TrelloCard data.SpeakerId data.ThreadId
+                return parseQuotedGuid result
+            }
+
+    module Threads = 
+        let postAsync (correspondence : CorrespondenceItem []) = 
+            async {
+                let! result = postAsync' threadsEndpoint correspondence 
+                return parseQuotedGuid result
+            }

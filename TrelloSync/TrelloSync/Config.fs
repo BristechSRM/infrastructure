f8ac3c3bd74645ed70a66a09cec65092d8983158
Jason Ebbin <jebbin@scottlogic.co.uk>
2016-05-16 16:10:41 +0100

    Add config and performImport flag

---
+++ TrelloSync/TrelloSync/Config.fs
@@ -0,0 +1,16 @@
+﻿module Config
+
+open System.Configuration
+open Serilog
+
+let getBoolConfig (key : string) =
+    match bool.TryParse <| ConfigurationManager.AppSettings.Item(key) with
+    | true , value -> value
+    | _ -> 
+        let message = sprintf "Could not parse configuration value for key: %s as boolean" key
+        Log.Fatal(key)
+        failwith message
+
+let useCache = getBoolConfig "UseCache"
+let performImport = getBoolConfig "PerformImport"
+    
--- TrelloSync/TrelloSync/Program.fs
+++ TrelloSync/TrelloSync/Program.fs
@@ -10,17 +10,9 @@ let main _ =
     JsonSettings.setDefaults()
     setupLogging()
     let cacheFilePath = @"trello-import.json"
-    let useCache = 
-        match ConfigurationManager.AppSettings.Item("UseCache").ToUpperInvariant() with 
-        | "TRUE" -> true
-        | "FALSE" -> false
-        | other -> 
-            let message = sprintf "Could not parse 'UseCache' configuration value: %s as boolean. Value must be one of true|TRUE|false|FALSE." other
-            Log.Fatal(message)
-            failwith message
     
     let trelloData = 
-        if useCache && File.Exists(cacheFilePath) then 
+        if Config.useCache && File.Exists(cacheFilePath) then 
             let trelloData = DataCache.load cacheFilePath
             Log.Information("Trello Data loaded from file: {file}",cacheFilePath)
             trelloData
@@ -31,5 +23,9 @@ let main _ =
             Log.Information("Trello Data download and parse complete. Saved to file: {file} for cache as well.", cacheFilePath)
             trelloData
     
-    let result = Import.importAll trelloData
+    let result = 
+        if Config.performImport then
+            Import.importAll trelloData
+        else
+            0
     0

7d79cd5146e449cddcc756c5e5fff6fcb7d683cd
Jason Ebbin <jebbin@scottlogic.co.uk>
2016-05-16 16:59:26 +0100

    Catch all and log

--- TrelloSync/TrelloSync/Program.fs
+++ TrelloSync/TrelloSync/Program.fs
@@ -4,24 +4,29 @@ open System.IO
 
 [<EntryPoint>]
 let main _ = 
-    JsonSettings.setDefaults()
-    setupLogging()
-    let cacheFilePath = @"trello-import.json"
+    try
+        JsonSettings.setDefaults()
+        setupLogging()
+        let cacheFilePath = @"trello-import.json"
     
-    let trelloData = 
-        if Config.useCache && File.Exists(cacheFilePath) then 
-            let trelloData = DataCache.load cacheFilePath
-            Log.Information("Trello Data loaded from file: {file}", cacheFilePath)
-            trelloData
-        else 
-            let trelloCred = Credentials.getTrelloCredentials()
-            let trelloData = BoardParser.parseBoardAsync trelloCred |> Async.RunSynchronously
-            DataCache.save trelloData cacheFilePath
-            Log.Information("Trello Data download and parse complete. Saved to file: {file} for cache as well.", cacheFilePath)
-            trelloData
+        let trelloData = 
+            if Config.useCache && File.Exists(cacheFilePath) then 
+                let trelloData = DataCache.load cacheFilePath
+                Log.Information("Trello Data loaded from file: {file}", cacheFilePath)
+                trelloData
+            else 
+                let trelloCred = Credentials.getTrelloCredentials()
+                let trelloData = BoardParser.parseBoardAsync trelloCred |> Async.RunSynchronously
+                DataCache.save trelloData cacheFilePath
+                Log.Information("Trello Data download and parse complete. Saved to file: {file} for cache as well.", cacheFilePath)
+                trelloData
     
-    let result = 
-        if Config.performImport then Import.importAll trelloData
-        else 0
-    
-    0
+        let result = 
+            if Config.performImport then Import.importAll trelloData
+            else 0
+        0
+    with
+        | ex -> 
+            Log.Fatal(ex.Message)
+            1
+

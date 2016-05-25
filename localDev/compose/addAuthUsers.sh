8489a7acc254726c7e5d53ee83583f6d6da50de8
Jason Ebbin <jebbin@scottlogic.co.uk>
2016-05-25 10:03:22 +0100

    change dynamodb ip in add auth users

--- localDev/compose/addAuthUsers.sh
+++ localDev/compose/addAuthUsers.sh
@@ -1,7 +1,7 @@
 uuid=$(uuidgen)
 # Set env variables for email, firstname & surname
 
-aws dynamodb put-item --endpoint-url http://dynamodb:7000 \
+aws dynamodb put-item --endpoint-url http://localhost:7000 \
   --table-name Auth.Users \
   --item \
       "{\"Id\":{\"S\":\"$uuid\"},\"Email\":{\"S\":\"$email\"},\"Firstname\":{\"S\":\"$firstname\"},\"Surname\":{\"S\":\"$surname\"}}"

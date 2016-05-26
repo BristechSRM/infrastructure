20dd940047a740312802682a2053e00b89b7c111
Kenneth Gray <kgray@scottlogic.co.uk>
2016-05-26 18:23:33 +0100

    Added ability to proxy services

---
+++ localDev/compose/utils/runProxy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BUILD_DIR="$DIR/../build"
+
+echo "Enter service id:"
+read SERVICE_ID
+SERVICE_PORT=$($DIR/servicePortMapping.sh $SERVICE_ID)
+
+echo "Enter host ip:"
+read HOST_IP
+
+sed "s/{PROXY_IP}/$HOST_IP/;s/{PORT}/$SERVICE_PORT/" $DIR/../config/proxy.default.conf > $BUILD_DIR/$SERVICE_ID.default.conf
+
+docker run -d --name $SERVICE_ID -v $BUILD_DIR/$SERVICE_ID.default.conf:/etc/nginx/conf.d/default.conf -p $SERVICE_PORT:$SERVICE_PORT nginx

4dfd759e5c8395f441824e9cb08c66aaa161f245
Paul Allen <pallen@scottlogic.co.uk>
2016-06-16 03:18:09 -0700

    secrets

--- cloudFormation/runAuth.sh
+++ cloudFormation/runAuth.sh
@@ -3,4 +3,4 @@
 PRIVATE_IP=$(ifconfig eth0 | awk '/inet addr/{split($2,a,":"); print a[2]}')
 DOCKER_HOST="tcp://${PRIVATE_IP}:2375"
 
-sudo docker -H ${DOCKER_HOST} run -d -p 8080:8080 -v /home/ubuntu/prd.auth.config:/service/Auth.exe.config bristechsrm/auth
+sudo docker -H ${DOCKER_HOST} run -d -p 8080:8080 -v /home/ubuntu/prd.auth.config:/service/Auth.exe.config -v /home/ubuntu/secrets.auth.config:/service/secrets.config bristechsrm/auth

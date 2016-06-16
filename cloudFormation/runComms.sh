ba7b047442850066cedc3de9c091ed789b2329ce
Paul Allen <pallen@scottlogic.co.uk>
2016-06-16 03:16:54 -0700

    secrets

--- cloudFormation/runComms.sh
+++ cloudFormation/runComms.sh
@@ -3,4 +3,4 @@
 PRIVATE_IP=$(ifconfig eth0 | awk '/inet addr/{split($2,a,":"); print a[2]}')
 DOCKER_HOST="tcp://${PRIVATE_IP}:2375"
 
-sudo docker -H ${DOCKER_HOST} run -d -p 8080:8080 -v /home/ubuntu/prd.comms.config:/service/Comms.exe.config bristechsrm/comms
+sudo docker -H ${DOCKER_HOST} run -d -p 8080:8080 -v /home/ubuntu/prd.comms.config:/service/Comms.exe.config -v /home/ubuntu/secrets.comms.config:/service/secrets.config bristechsrm/comms

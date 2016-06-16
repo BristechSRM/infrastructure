48fa61964025a4f7deaf1952655d403a618395d1
Paul Allen <pallen@scottlogic.co.uk>
2016-06-16 02:24:17 -0700

    more

---
+++ cloudFormation/runAuth.sh
@@ -0,0 +1,6 @@
+#!/bin/sh -eu
+
+PRIVATE_IP=$(ifconfig eth0 | awk '/inet addr/{split($2,a,":"); print a[2]}')
+DOCKER_HOST="tcp://${PRIVATE_IP}:2375"
+
+sudo docker -H ${DOCKER_HOST} run -d -p 8080:8080 -v /home/ubuntu/prd.auth.config:/service/Auth.exe.config bristechsrm/auth

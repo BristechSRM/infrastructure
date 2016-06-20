b2fe5b558736503088d6a79dd9bd4a77bf911db9
Paul Allen <pallen@scottlogic.co.uk>
2016-06-20 04:08:37 -0700

    Relocate.  Add swarm master access group

--- dataCenter/services/createStack.sh
+++ dataCenter/services/createStack.sh
@@ -1,21 +1,23 @@
 #!/bin/sh -eu
 
-if [ "$#" -ne 6 ]; then
-    echo "Usage: $0 environment-tag vpc-id subnet-id authEIP apiEIP frontendEIP"
+if [ "$#" -ne 7 ]; then
+    echo "Usage: $0 environment-tag vpc-id subnet-id concourse-subnet-cidr authEIP apiEIP frontendEIP"
     exit 1
 fi
 
 ENVIRONMENT_TAG=$1
 VPC_ID=$2
 SUBNET_ID=$3
-AUTH_EIP=$4
-API_EIP=$5
-FRONTEND_EIP=$6
+CONCOURSE_SUBNET_CIDR=$4
+AUTH_EIP=$5
+API_EIP=$6
+FRONTEND_EIP=$7
 
 aws cloudformation create-stack --stack-name ${ENVIRONMENT_TAG}-srm-all --template-body file://srm-all.json --parameters \
     ParameterKey=environment,ParameterValue=${ENVIRONMENT_TAG} \
     ParameterKey=vpcId,ParameterValue=${VPC_ID} \
-    ParameterKey=subnet,ParameterValue=${SUBNET_ID} \
+    ParameterKey=servicesSubnetId,ParameterValue=${SUBNET_ID} \
+    ParameterKey=concourseSubnetCIDR,ParameterValue=${CONCOURSE_SUBNET_CIDR} \
     ParameterKey=authEIP,ParameterValue=${AUTH_EIP} \
     ParameterKey=apiEIP,ParameterValue=${API_EIP} \
     ParameterKey=frontendEIP,ParameterValue=${FRONTEND_EIP}

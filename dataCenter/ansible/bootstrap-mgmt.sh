30acd6c1a00d3a9467b15c9e0e3adc98f77b7ac9
Paul Allen <pallen@scottlogic.co.uk>
2016-07-12 11:49:25 +0100

    Bare nodes

--- dataCenter/ansible/bootstrap-mgmt.sh
+++ dataCenter/ansible/bootstrap-mgmt.sh
@@ -4,3 +4,12 @@ apt-get -y install software-properties-common
 apt-add-repository -y ppa:ansible/ansible
 apt-get update
 apt-get -y install ansible
+
+cat << EOF >> /etc/hosts
+
+10.0.15.11 auth
+10.0.15.12 comms
+10.0.15.13 sessions
+10.0.15.14 gateway
+10.0.15.15 frontend
+EOF

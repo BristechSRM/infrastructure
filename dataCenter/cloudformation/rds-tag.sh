0a123e26e779ebf6d210ea2cfd2a7c0acac59661
Paul Allen <pallen@scottlogic.co.uk>
2016-08-01 16:08:49 +0100

    Instance, username, password parameters

--- dataCenter/cloudformation/rds-tag.sh
+++ dataCenter/cloudformation/rds-tag.sh
@@ -1,15 +1,23 @@
 #!/bin/sh -eu
 
-if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 environment-tag vpcId subnetGroupName"
+if [ "$#" -ne 7 ]; then
+    echo "Usage: $0 environment-tag dbInstanceName username password vpcId dbSubnetGroupName servicesSubnetCIDR"
     exit 1
 fi
 
 ENVIRONMENT_TAG=$1
-VPC_ID=$2
-SUBNET_GROUP_NAME=$3
+INSTANCE_NAME=$2
+USERNAME=$3
+PASSWORD=$4
+VPC_ID=$5
+DB_SUBNET_GROUP_NAME=$6
+SERVICES_SUBNET_CIDR=$7
 
 aws cloudformation create-stack --stack-name ${ENVIRONMENT_TAG}-rds --template-body file://rds-tag.json --parameters \
     ParameterKey=environment,ParameterValue=${ENVIRONMENT_TAG} \
+    ParameterKey=instanceName,ParameterValue=${INSTANCE_NAME} \
+    ParameterKey=masterUsername,ParameterValue=${USERNAME} \
+    ParameterKey=masterPassword,ParameterValue=${PASSWORD} \
     ParameterKey=vpcId,ParameterValue=${VPC_ID} \
-    ParameterKey=subnetGroupName,ParameterValue=${SUBNET_GROUP_NAME}
+    ParameterKey=dbSubnetGroupName,ParameterValue=${DB_SUBNET_GROUP_NAME} \
+    ParameterKey=servicesSubnetCIDR,ParameterValue=${SERVICES_SUBNET_CIDR}

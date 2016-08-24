2e06e40e8f90f7233307de1c22c52e869a80a99f
Paul Allen <palllen>
2016-08-24 15:30:57 +0100

    A swarm of 3x t2.nanos

--- dataCenter/cloudformation/bootstrap-driver.sh
+++ dataCenter/cloudformation/bootstrap-driver.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -eu
 
+apt-get update
 apt-get install -y python-pip
 pip install --upgrade pip
 pip install awscli
--- dataCenter/cloudformation/srm-tag.sh
+++ dataCenter/cloudformation/srm-tag.sh
@@ -1,17 +1,19 @@
 #!/bin/sh -eu
 
-if [ "$#" -ne 4 ]; then
-    echo "Usage: $0 environment-tag vpc-id subnet-id concourse-subnet-cidr"
+if [ "$#" -ne 5 ]; then
+    echo "Usage: $0 environment-tag vpc-id subnet-id subnet-cidr concourse-subnet-cidr"
     exit 1
 fi
 
 ENVIRONMENT_TAG=$1
 VPC_ID=$2
 SUBNET_ID=$3
-CONCOURSE_SUBNET_CIDR=$4
+SUBNET_CIDR=$4
+CONCOURSE_SUBNET_CIDR=$5
 
 aws cloudformation create-stack --stack-name ${ENVIRONMENT_TAG}-srm --template-body file://srm-tag.json --parameters \
     ParameterKey=environment,ParameterValue=${ENVIRONMENT_TAG} \
     ParameterKey=vpcId,ParameterValue=${VPC_ID} \
     ParameterKey=servicesSubnetId,ParameterValue=${SUBNET_ID} \
+    ParameterKey=servicesSubnetCIDR,ParameterValue=${SUBNET_CIDR} \
     ParameterKey=concourseSubnetCIDR,ParameterValue=${CONCOURSE_SUBNET_CIDR}

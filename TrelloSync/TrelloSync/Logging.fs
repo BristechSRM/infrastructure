3ddac2052c2a437e11cf71b2001a1a9d16063f5a
Jason Ebbin <jebbin@scottlogic.co.uk>
2016-08-26 11:17:12 +0100

    Remove old trello Sync. Rewrite available at: https://github.com/BristechSRM/TrelloMigrate

--- TrelloSync/TrelloSync/Logging.fs
+++
@@ -1,7 +0,0 @@
-﻿module Logging
-
-open Serilog
-
-let setupLogging () =
-    Log.Logger <- LoggerConfiguration().ReadFrom.AppSettings().CreateLogger()
-    Log.Logger.Information("Serilog logging initialised") 

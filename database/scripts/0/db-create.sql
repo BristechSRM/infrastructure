2515283719270d7b6e8c04d73d730bd74077d91f
Jason Ebbin <jebbin@scottlogic.co.uk>
2016-10-07 14:09:39 +0100

    Adjust image url length

--- database/scripts/0/db-create.sql
+++ database/scripts/0/db-create.sql
@@ -6,7 +6,7 @@ CREATE TABLE `profiles` (
   `id` char(36) NOT NULL,
   `forename` varchar(255) NOT NULL,
   `surname` varchar(255) NOT NULL,
-  `imageUrl` varchar(255) DEFAULT NULL,
+  `imageUrl` varchar(2000) DEFAULT NULL,
   `bio` text,
   `isAdmin` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
